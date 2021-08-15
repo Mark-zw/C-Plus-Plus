759b8d321d3a5b8a79f12d34e2b76a2b1c8b4fd9
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-15 10:10:39 +0800

    Add files via upload

---
+++ class_0815/class_0815.cpp
@@ -0,0 +1,53 @@
+//#include<iostream>
+////using namespace std;//C++库中的所有东西都是放到std命名空间中
+//using  std::cout;
+//using  std::endl;
+//int main()
+//{
+//	cout << "hello world!" << endl;
+//	//std::cout << "hello world!"<< std::endl;
+//	int i = 1;
+//	double d = 1.111;
+//	cout << i << endl;
+//	cout << d << endl;
+//	std::cin >> i;
+//	std::cin >> d;
+//	cout << i << "\n" << d << endl;
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+////全缺省
+//void Func1(int a = 0,int b = 1,int c = 2)
+//{
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//}
+////半缺省
+//void Func2(int a, int b, int c = 30)
+//{
+//	cout << a << endl;
+//	cout << b << endl;
+//	cout << c << endl;
+//}
+//int main()
+//{
+//	Func1();
+//	Func2(10,20);
+//	cout << "Practice make perfect!\n";
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+void  fun(int a, double d)
+{}
+void  fun(double d, int a)
+{}
+void fun(char a,int b)
+{}
+int main()
+{
+	return 0;
+}
